5df65a3756e849d26e66d7db6fcf14cc90c6a68b
Joao Machado <machado000@gmail.com>
2024-10-31 09:59:23 -0300

    Initial commit

---
+++ generate_preview_page.py
@@ -0,0 +1,21 @@
+#! /home/nginx/preview_generator/.venv/bin/python
+import csv
+from jinja2 import Environment, FileSystemLoader
+
+# Load the CSV file
+data = []
+with open('banner_list.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        data.append(row)
+
+# Set up Jinja2 environment
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('template_nova.html')
+
+# Render the template with data
+output = template.render(rows=data)
+
+# Save the output to a file
+with open('index.html', 'w', encoding='utf-8') as f:
+    f.write(output)
---
+++ transicao_energetica_728x90/script.js
@@ -0,0 +1,72 @@
+var animations = [
+{
+        
+        id: '#i02',
+        duration: 400,
+        delayBefore: 0,
+        delayAfter: 3500,
+        animations: { opacity: 1, top:0, left:0}
+    },
+    {
+        
+        id: '#i03',
+        duration: 500,
+        delayBefore: 0,
+        delayAfter: 0,
+        animations: { opacity: 1, top:0, left:0}
+    },{
+        
+        id: '#i04',
+        duration: 350,
+        delayBefore: 0,
+        delayAfter: 0,
+        animations: { opacity: 1, top:0, left:0}
+    },{
+        
+        id: '#i05',
+        duration: 350,
+        delayBefore: 0,
+        delayAfter: 4000,
+        animations: { opacity: 1, top:0, left:0}
+    },{
+        
+        id: '#i06',
+        duration: 350,
+        delayBefore: 0,
+        delayAfter: 0,
+        animations: { opacity: 1, top:0, left:0}
+    },{
+        
+        id: '#i07',
+        duration: 350,
+        delayBefore: 0,
+        delayAfter: 3000,
+        animations: { opacity: 1, top:0, left:0}
+    },
+    [
+        
+       {
+            id: '#i07',
+            duration: 200,
+            delayBefore: 0,
+            delayAfter: 400,
+            animations: { opacity: 0, top:"10%", left:0,}
+        },
+],
+    {
+        
+        id: '#i08',
+        duration: 300,
+        delayBefore: 0,
+        delayAfter: 0,
+        animations: { opacity: 1, top:0, left:0}
+    },{
+        
+        id: '#i09',
+        duration: 300,
+        delayBefore: 0,
+        delayAfter: 3500,
+        animations: { opacity: 1, top:0, left:0}
+    },
+]
+new YAnimation(animations, { loop: false, clearAfterEnd: 0 }).startAnimation()
