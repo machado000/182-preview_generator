4f4d8623f7b1811fc7024f800e1805502e8b3531
Joao Machado <machado000@gmail.com>
2025-07-23 11:06:33 -0300

    Refactor template rendering to include additional parameters for cliente, campanha, and periodo

--- generate_preview_page.py
+++ generate_preview_page.py
@@ -14,7 +14,12 @@ env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('./templates/template_nova.html')
 
 # Render the template with data
-output = template.render(rows=data)
+output = template.render(
+    cliente='BANCO DO BRASIL',
+    campanha='SKATE - POS-EVENTO',
+    periodo='JULHO/2025',
+    rows=data
+)
 
 # Save the output to a file
 with open('index.html', 'w', encoding='utf-8') as f:
